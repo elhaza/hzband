9118d6da4f25ac8f8b3dd7a20639e16eec0d2e77
Haza Gomez <elhaza@gmail.com>
2025-09-08 00:34:27 -0700

    fix: Compile assets with npm (currently using Blade views, but they will be migrated soon)

---
+++ resources/js/app.ts
@@ -0,0 +1,8 @@
+// JS base para Blade
+import '../css/app.css';
+
+// Si usas Alpine u otro, impórtalo aquí.
+// Ejemplo:
+// import Alpine from 'alpinejs';
+// window.Alpine = Alpine;
+// Alpine.start();
